2d9c0fde5da38fbc9f96eb383b7ccfc1bb8380c8
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-21 19:20:48 -0500

    :bookmark: v1.0.0

---
+++ scripts/node_support.ts
@@ -0,0 +1,55 @@
+import { build } from "https://deno.land/x/dnt@0.22.0/mod.ts";
+
+const publisher = {
+  name: "Eliaz Bobadilla",
+  username: "ultirequiem",
+  email: "eliaz.bobadilladev@gmail.com",
+  site: "https://ultirequiem.com",
+} as const;
+
+const packageConfig = {
+  name: "@ultirequiem/stable-fn",
+  repoName: "is-number",
+  version: "1.0.0",
+  description: "Ensure the same input always produces the same output",
+  keywords: ["testing", "assert"],
+  license: "MIT",
+  homepage: "https://stable-fn.js.org",
+} as const;
+
+await build({
+  entryPoints: ["./mod.ts"],
+
+  outDir: "./node",
+
+  shims: {
+    deno: true,
+  },
+
+  package: {
+    name: packageConfig.name,
+    description: packageConfig.description,
+
+    version: packageConfig.version,
+
+    license: packageConfig.license,
+
+    funding: {
+      type: "patreon",
+      url: `https://www.patreon.com/${publisher.username}`,
+    },
+
+    repository: `github:${publisher.username}/${packageConfig.repoName}`,
+    homepage: packageConfig.homepage,
+
+    bugs: {
+      url: `https://github.com/${publisher.username}/${packageConfig.repoName}/issues`,
+      email: publisher.email,
+    },
+
+    keywords: packageConfig.keywords,
+  },
+});
+
+Deno.copyFileSync("license", "node/LICENSE");
+Deno.copyFileSync("readme.md", "node/README.md");
