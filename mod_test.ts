e7ceaeaecbf73aa79aa145270781672903643ced
GitHub <noreply@github.com>
2022-04-01 00:16:01 +0000

    chore(deps): update deno dependencies

--- mod_test.ts
+++ mod_test.ts
@@ -1,6 +1,6 @@
 import { stableFunction } from "./mod.ts";
 
-import { assert } from "https://deno.land/std@0.128.0/testing/asserts.ts";
+import { assert } from "https://deno.land/std@0.133.0/testing/asserts.ts";
 
 Deno.test("[stableFunction]", () => {
   assert(stableFunction(() => true));
